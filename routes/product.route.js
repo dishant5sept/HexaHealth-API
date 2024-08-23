936e8db6118b1e4ff2de42fd3737ecffe7f3159c
dishant5sept <104493446+dishant5sept@users.noreply.github.com>
2024-08-23 19:02:56 +0530

    Add files via upload

---
+++ routes/product.route.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const Product = require("../models/product.model.js");
+const router = express.Router();
+const {getProducts, getProduct, createProduct, updatedProduct, deleteProduct} = require('../Controllers/product.controller.js')
+
+router.get('/',getProducts);
+router.get("/:id",getProduct);
+router.post("/", createProduct);
+
+//update a product
+
+router.put("/:id", updatedProduct);
+
+//delete a product
+
+router.delete("/:id", deleteProduct);
+
+
+module.exports = router;
